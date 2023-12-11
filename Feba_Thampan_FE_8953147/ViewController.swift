05491bf4350d9d9d8ff7e6147363299789abe971
user234888 <user234888@NY551.local>
2023-12-11 16:14:59 -0500

    Tab bar and home page map

--- Feba_Thampan_FE_8953147/ViewController.swift
+++ Feba_Thampan_FE_8953147/ViewController.swift
@@ -7,16 +7,11 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController,CLLocationManagerDelegate {
     
-    @IBOutlet weak var weatherLocation: UILabel!
-    @IBOutlet weak var currentWeather: UILabel!
-    @IBOutlet weak var weatherImage: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var humidityLabel: UILabel!
-    @IBOutlet weak var windLabel: UILabel!
-    
+    @IBOutlet weak var currentLocation: MKMapView!
     
     let locationManager = CLLocationManager()
 
@@ -34,55 +29,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
             }
         }
         
-        
-
-        
-        func getWeatherAPI(latatiduCord: String, longitudeCord: String) {
-            guard let url = URL(string:"https://api.openweathermap.org/data/2.5/weather?lat=\(latatiduCord)&lon=\(longitudeCord)&appid=3c9f7ef5c40f00e9188febcb2a8024ae&units=metric") else {
-                return}
-            let task = URLSession.shared.dataTask(with: url) { [self]
-                data, response, error in
-                /* print(data!)
-                 if let data = data, let string = String(data: data, encoding: .utf8){
-                 print(string)*/
-                if let data = data {
-                    let jsonDecoder = JSONDecoder()
-                    do {
-                        let jsonData = try jsonDecoder.decode(Weather.self, from: data)
-                        print(jsonData.name)
-                        print(jsonData.coord)
-                        Task {@MainActor in
-                            self.weatherLocation.text = jsonData.name
-                            self.currentWeather.text = jsonData.weather[0].main
-                            self.temperatureLabel.text = String(jsonData.main.temp) + " °"
-                            self.humidityLabel.text = "Humidity : "+String(jsonData.main.humidity)+"%"
-                            // convert m/h to km/h by multiplying 3.6
-                            let windSpeedKMH = String(format: "%.f", jsonData.wind.speed * 3.6)
-                            self.windLabel.text = "Wind : "+String(windSpeedKMH)+"km/h"
-                            let iconCode = jsonData.weather[0].icon
-                            print(iconCode)
-                            let iconUrl = URL(string: "https://openweathermap.org/img/wn/\(iconCode).png")!
-                            self.WeatherIcon(IconURL: iconUrl, weatherIconView: self.weatherImage)
-                        }
-                        
-                    } catch {
-                        print("SOME ERROR")
-                    }
-                }
-            }
-            task.resume()
-        }
-            //method to convert URL to Image
-            func WeatherIcon(IconURL: URL, weatherIconView: UIImageView) {
-                    let task = URLSession.shared.dataTask(with: IconURL) { data, _, _ in
-                        guard let data = data else { return }
-                        DispatchQueue.main.async { // Make sure you're on the main thread here
-                            weatherIconView.image = UIImage(data: data)
-                        }
-                    }
-                    task.resume()
-            }
-    
+           
     
     func navigateToMapScene(cityName: String){
         let storyboard = UIStoryboard(name: "Main", bundle: nil) // Use your storyboard name
@@ -123,27 +70,6 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         
     }
         
-       /* func geocodeCityAndCallAPI(_ cityName: String) {
-            let geocoder = CLGeocoder()
-
-            geocoder.geocodeAddressString(cityName) { (placemarks, error) in
-                if let error = error {
-                    print("Geocoding error: \(error.localizedDescription)")
-                    // Handle geocoding error if needed
-                } else if let location = placemarks?.first?.location {
-                    // Use the obtained coordinates
-                    let latitude = location.coordinate.latitude
-                    let longitude = location.coordinate.longitude
-
-                    // Do something with the latitude and longitude
-                    print("City: \(cityName), Latitude: \(latitude), Longitude: \(longitude)")
-
-                    // Call weather API with city coordinates
-                    self.getWeatherAPI(latatiduCord: String(latitude), longitudeCord: String(longitude))
-                }
-            }
-        }*/
-
     
     @IBAction func showDiscoverLocationAlert(_ sender: Any) {
    
@@ -189,6 +115,25 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
 
     }
     
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let userLocation = locations.last?.coordinate else {
+            return
+        }
+
+        
+        let region = MKCoordinateRegion(center: userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        currentLocation.setRegion(region, animated: true)
+
+        // Add a pin to mark the user's location
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = userLocation
+        annotation.title = "Current Location"
+        currentLocation.addAnnotation(annotation)
+
+        // Stop updating location
+        locationManager.stopUpdatingLocation()
+    }
     
     
 }
--- Feba_Thampan_FE_8953147/WeatherViewController.swift
+++ Feba_Thampan_FE_8953147/WeatherViewController.swift
@@ -162,14 +162,6 @@ let getWeatherIconUrl = "https://openweathermap.org/img/wn/"
 
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
